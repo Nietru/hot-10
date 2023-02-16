97619a06d6e69436a9fb66a4a51f998bdc65cc67
Tifannie Truman <nietru143@gmail.com>
2023-02-16 16:06:30 -0500

    what?

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -14,6 +14,7 @@ var artistName = searchBox.value;
 function addToHistory() {
   // Pushing searchbox value to artists array, then storing array in localStorage
   userInput.push(searchBox.value);
+  artistsArray.append(artistName);
   localStorage.setItem("Button", JSON.stringify(artistsArray));
 }
 
